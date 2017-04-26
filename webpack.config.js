129bc4eb586b1f10b1f55ba8012284d2fdbcd067
Varun V Subramanian <vsubramanian@aconex.com>
2017-04-26 23:57:55 +0530

    Add webpack, eslint and less

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  devtool: 'inline-sourcemap',
+  context: path.resolve(__dirname, './src'),
+  entry: {
+    app: './index.js',
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].bundle.js',
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, './src'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loaders: [
+          {
+            loader: 'babel-loader', 
+            query: {
+              presets: ['es2015']
+            }
+          },
+          'eslint-loader'
+        ],
+      },
+      {
+        test: /\.less$/,
+        use: [{
+          loader: 'style-loader'
+        }, {
+          loader: 'css-loader'
+        }, {
+          loader: 'less-loader'
+        }]
+      }
+    ]
+  }
+};
