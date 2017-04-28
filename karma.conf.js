6d0bd078dc4bf9a8d757972bd505d3c82f8cc59c
Varun V Subramanian <vsubramanian@aconex.com>
2017-04-28 23:13:53 +0530

    Fix karma config pointing to wrong js file

--- karma.conf.js
+++ karma.conf.js
@@ -15,7 +15,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'dist/app.bundle.js',
+      'docs/app.bundle.js',
       'node_modules/angular-mocks/angular-mocks.js',
       'src/**/*spec.js'
     ],
